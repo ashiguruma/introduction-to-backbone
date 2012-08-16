ea88a25e4b3a69b04fe1b9a8c81aea09501be414
ashiguruma <tom.loudon@axisto.com>
2012-08-16 17:39:33 +0100

    Commented the application.js file

--- js/application.js
+++ js/application.js
@@ -1,3 +1,10 @@
+/**
+ * Application Namespace
+ *
+ * This namespace helps keep our collections and views organised,
+ * it also stops the window namespace from getting cluttered as the
+ * app grows in size
+ */
 var app = {
     collections: {},
     views: {}
